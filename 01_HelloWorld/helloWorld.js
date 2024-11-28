3396055d244e5a2d796d2f990299251a8ff6100f
Abhijeet Kumar <abhijeetkumar3245@gmail.com>
2024-11-28 21:15:38 +0530

    01_HelloWorld

---
+++ 01_HelloWorld/helloWorld.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+console.log(9+6);
+
+// console.log(alert('hey'));
+// console.log(window);
+
+//This alert and window is not working because the dom and window is removed from the node it works only in the browser, beacause it is related with the UI.
+
+
+
